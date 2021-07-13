6150a4a39b586c54c0ee6818acedcc751a6157e3
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-13 13:44:36 -0500

    Math inside for loops

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = 0; i < length; i++)
+        {
+            string line = new String(' ', i);
+            Console.WriteLine("{0}\\", line);
+        }
+        Console.WriteLine();
+    }
+}
