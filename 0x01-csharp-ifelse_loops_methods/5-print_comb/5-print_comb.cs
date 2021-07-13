ddeefd57ce336940862923751dbda57495921ca9
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-13 11:35:02 -0500

    Practice with string format

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+                Console.Write("{0:N}, ", String.Format("{0:00}", i));
+            Console.WriteLine("99");
+        }
+    }
+}
