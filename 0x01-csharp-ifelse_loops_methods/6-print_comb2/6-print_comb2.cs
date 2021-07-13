ad6f31ec1a3785c0e1f740583896f499e6e6c53a
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-13 11:46:11 -0500

    Prints unique combinations of ints

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            for (x = 0; x <= 9; x++)
+            {
+                for (y = x + 1; y <= 9; y++)
+                    if (x == 8 && y == 9)
+                        Console.WriteLine("{0}{1}", x, y);
+                    else
+                        Console.Write("{0}{1}, ", x, y);
+                        
+            }
+        }
+    }
+}
