dedb7f28d5c272c90e99982e447fc9834e93fca1
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-13 13:20:49 -0500

    Repeating character n times

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            if (length > 0)
+            {
+                string line = new String('_', length);
+                Console.WriteLine(line);
+            }
+            else
+                Console.WriteLine();
+        }
+    }
