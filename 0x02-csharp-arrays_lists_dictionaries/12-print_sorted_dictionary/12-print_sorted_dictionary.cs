85c60fd3cc3fb005b9fc16e8f67b8d8e3a7c896a
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-14 16:56:37 -0500

    Using linq to sort

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        foreach (KeyValuePair<string, string> entry in myDict.OrderBy(x => x.Key))
+            Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+    }
+}
