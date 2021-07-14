66c146e8ea059655ef525298665d4136a45d1ffb
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-14 17:11:01 -0500

    Returns highest score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        var ordered = myList.OrderByDescending(x => x.Value);
+        foreach(KeyValuePair<string, int> elem in ordered)
+            return elem.Key;
+        return null;
+    }
+}
