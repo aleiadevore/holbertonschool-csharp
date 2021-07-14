a89b216e308bf2ba3f5ba6883c9112007d2d95e4
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-14 10:47:13 -0500

    Creating new array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] a1 = new int[size];
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return a1;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            a1[i] = i;
+            if (i != size - 1)
+                Console.Write("{0} ", i);
+            else
+                Console.WriteLine(i);
+        }
+        
+        return a1;
+    }
+}
