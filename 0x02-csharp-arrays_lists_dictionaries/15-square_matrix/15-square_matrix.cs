a1c3b3020b796365bb9f398587db88f4aa58a146
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-14 17:55:35 -0500

    Squares elems of matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {
+            int rows = myMatrix.GetLength(0);
+            int cols = myMatrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    myMatrix[i, j] *= myMatrix[i, j];
+                }
+            }
+            return myMatrix;
+        }
+    }
