0fc4c24f4444e244fecc959a421354e2ccb4fb97
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-14 11:56:54 -0500

    This could have been done with Enumerate if we could use a modern .net

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+class List         
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = new List<int>();
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return newList;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            newList.Add(i);
+            if (i != size - 1)
+                Console.Write("{0} ", i);
+            else
+                Console.WriteLine(i);
+        }
+        
+        return newList;
+
+    }
+}
