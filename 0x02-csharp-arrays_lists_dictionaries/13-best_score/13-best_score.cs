4d222d669f8cc21f1fdce5c861904fe79fdf1776
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-14 17:14:21 -0500

    accounting for empty dict with none

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -7,7 +7,7 @@ class Dictionary
     public static string BestScore(Dictionary<string, int> myList)
     {
         if (myList.Count == 0)
-            return "";
+            return null;
         var ordered = myList.OrderByDescending(x => x.Value);
         foreach(KeyValuePair<string, int> elem in ordered)
             return elem.Key;
