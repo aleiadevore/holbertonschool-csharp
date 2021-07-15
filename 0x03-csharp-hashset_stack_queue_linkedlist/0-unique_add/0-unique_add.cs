0d04028d25bdfba24e14d5d8ccec91ec2d54e89c
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 12:18:19 -0500

    Adds all unique in list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var uni = myList.Distinct();
+
+        int i = 0;
+
+        foreach (int n in uni)
+        {
+            i += n;
+        }
+
+        return i;
+    }
+}
