484af0081a64dd1960d25399359e45cd6282194e
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 14:01:45 -0500

    Creates and prints linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> nList = new LinkedList<int>();
+
+        if (size < 0)
+            return nList;
+        
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(i);
+            nList.AddLast(i);
+        }
+        return nList;
+        
+    }
+}
