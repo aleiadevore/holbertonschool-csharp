9f47b84e1e98e3d7fa561bdd60263dc0892bedf6
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 13:43:00 -0500

    Manipulated queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+    Boolean check = aQueue.Contains(search);
+
+        // Number of elements in stack
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        // Prints top of stack
+        if (aQueue.Count > 0)
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+        
+        // Prints if contains search item search
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, check);
+
+        // If aQueue contains the given item search, remove all items up to and including search;
+        // otherwise, leave aQueue as is
+        string[] aCopy = new string[aQueue.Count];
+        aQueue.CopyTo(aCopy, 0);
+
+        if (check == true)
+        {
+            foreach (string item in aCopy)
+            {
+                aQueue.Dequeue();
+                if (item == search)
+                    break;
+            }
+        }
+
+        // Add a new given item newItem to aQueue
+        if (newItem != null)
+            aQueue.Enqueue(newItem);
+        
+        return aQueue;
+
+    }
+}
