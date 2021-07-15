e555c029d70494150e2976f2d7ce6aedf9eb8712
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 13:08:09 -0500

    Returns list of only differences

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        // create hash of each to get only unique numbers
+        HashSet<int> hs1 = new HashSet<int>(list1);
+        HashSet<int> hs2 = new HashSet<int>(list2);
+
+        List<int> ans = new List<int>();
+        hs1.SymmetricExceptWith(hs2);
+
+        foreach (int i in hs1)
+        {
+            ans.Add(i);
+        }
+        ans.Sort();
+        return ans;
+    }
+}
