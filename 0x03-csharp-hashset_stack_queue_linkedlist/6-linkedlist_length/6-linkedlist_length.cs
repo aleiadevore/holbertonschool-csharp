b3d957247b715afd9b986783ed7498ca4a3787e1
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 14:05:03 -0500

    Gets size of linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int i = 0;
+        
+        foreach (int item in myLList)
+            i++;
+
+        return i;
+        
+    }
+}
