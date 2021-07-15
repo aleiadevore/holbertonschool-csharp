e03264366c3f362e086e7a17aee23087a330d9ca
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 14:39:41 -0500

    Accounted for negative index

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -6,7 +6,7 @@ class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        if (n > myLList.Count)
+        if (n >= myLList.Count || myLList.Count == 0 || myLList == null || n < 0)
             return 0;
         return myLList.ElementAt(n);
     }
