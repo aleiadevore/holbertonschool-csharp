6486eb8a1328887518b7691ef4b7a725fef1d030
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 16:05:29 -0500

    Won't work if multiple of same values

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i = 0;
+        LinkedListNode<int> current;
+
+        if (myLList == null || myLList.Count == 0)
+            return;
+        if (index == 0)
+            myLList.RemoveFirst();
+        else if (index == myLList.Count -1)
+            myLList.RemoveLast();
+        else
+        {
+            int n = 0;
+            for (current = myLList.First; current != null; current = current.Next)
+            {
+                if (i == index)
+                {
+                    n = current.Value;
+                    break;
+                }
+                i++;
+            }
+            if (n != 0)
+                myLList.Remove(n);
+        }
+    }
+}
