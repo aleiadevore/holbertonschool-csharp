3effbee2797a8f54f45d5cb4f5377e24312a6b42
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 12:56:50 -0500

    Old version with logic questions: If the console write is correctly printing true/false, why is the if statement accepting it as true every time?

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/old1/1-common_elements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // create hash of each to get only unique numbers
+        HashSet<int> hs1 = new HashSet<int>(list1);
+        HashSet<int> hs2 = new HashSet<int>(list2);
+
+        List<int> ans = new List<int>();
+
+        // if element in hash 1 also in hash 2, add to new list
+        foreach (int item in hs1)
+        {
+            Console.WriteLine(hs2.Contains(item));
+            if (hs2.Contains(item));
+            {
+                ans.Add(item);
+            }
+        }
+
+        // return new list
+        return ans;
+    }
+}
