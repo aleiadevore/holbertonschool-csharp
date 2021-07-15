4ef6fb9352d19117f1ff86e8857e45323d3bd9d2
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 13:02:02 -0500

    Finds list items in common

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // create hash of each to get only unique numbers
+        HashSet<int> hs1 = new HashSet<int>(list1);
+        HashSet<int> hs2 = new HashSet<int>(list2);
+
+        List<int> ans = new List<int>();
+        hs1.IntersectWith(hs2);
+
+        foreach (int i in hs1)
+        {
+            ans.Add(i);
+        }
+
+        return ans;
+    }
+}
