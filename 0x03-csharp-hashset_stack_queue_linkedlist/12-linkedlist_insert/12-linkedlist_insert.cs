e5a7ddd8c15901b3982387424ec871466ecc3fb8
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-15 15:34:58 -0500

    Inserts into sorted linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current;
+        LinkedListNode<int> nnode;
+
+        for (current = myLList.First; current != null; current = current.Next)
+        {
+            if (current.Value > n)
+            {
+                myLList.AddBefore(current, n);
+                return current.Previous;
+            }
+            else if (current.Next != null)
+            {
+                if (current.Next.Value > n && current.Value > n)
+                {
+                    nnode = myLList.AddAfter(current, n);
+                    return current.Previous;
+                }
+            }
+            else if (current.Value < n)
+            {
+                nnode = myLList.AddAfter(current, n);
+                return current.Next;
+            }
+        }
+        return null;
+    }
+}
