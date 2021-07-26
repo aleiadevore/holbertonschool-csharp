f82358fe7e13a5d80524288c458445e26a233023
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-26 14:04:09 -0500

    Overrides ToString with custom output

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public Dog(string n, float a, string o, Rating r)
+    {
+        name = n;
+        age = a;
+        owner = o;
+        rating = r;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public override string ToString() => $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+}
