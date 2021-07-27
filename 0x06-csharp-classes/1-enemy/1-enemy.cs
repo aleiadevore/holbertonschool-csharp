1ca78a121aee3904ad8911fef57fce738477bdb3
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-27 12:41:18 -0500

    Adding public field and constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This namespace creates classes of enemies.
+    /// </summary>
+
+    public class Zombie
+    {
+        /// <summary>
+        /// This class creates a Zombie and sets its health to 0.
+        /// </summary>
+
+        public int health;
+        /// <summary>
+        /// Creates public health field
+        /// </summary>
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
