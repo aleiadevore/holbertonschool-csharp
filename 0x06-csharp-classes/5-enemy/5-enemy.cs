5233fb6491ba8fcc88522a350ae9f12efc8df180
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-27 13:46:04 -0500

    Created custom ToString

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This class creates a Zombie and sets its health to 0.</summary>
+    public class Zombie
+    {
+
+        private int health;
+        string name = "(No name)";
+
+        /// <summary>Constructs new zombie with health of 0</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>Sets value of health. If value is less than 0, an error is thrown.</summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                health = value;
+            else
+                throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+
+        /// <summary>Gets and sets the value of private field name</summary>
+        public string Name { get => name; set => name = value;}
+
+        /// <summary>Gets value of Zombie health</summary> 
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>Overrides ToString with custom message</summary>
+        public override string ToString() => $"Zombie Name: {name} / Total Health: {health}";
+    }
+}
