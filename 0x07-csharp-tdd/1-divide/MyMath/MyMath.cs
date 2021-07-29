de1bcf52c2873524cd69af2d6ffde4ed47463a74
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-29 15:45:20 -0500

    Tests not written. Only code

---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Creates class of operations</summary>
+    public class Operations
+    {
+        ///<summary>Divides all elements of a matrix</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            int[,] newMatrix = new int[,];
+
+            if (matrix == null)
+                return null;
+            foreach (int i in matrix)
+            {
+                try
+                {
+                    newMatrix.Add(i / number);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Num cannot be 0");
+                    return null;
+                }
+            }
+            
+        }
+    }
+}
