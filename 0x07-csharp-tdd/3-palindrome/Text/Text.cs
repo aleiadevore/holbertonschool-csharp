962996cc4ef3cb67b849261f533269f93184f71a
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-29 16:49:24 -0500

    Created function, no tests

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Checks elements of string</summary>
+    public class Str
+    {
+        ///<summary>Checks if string is palindrome</summary>
+        public static bool IsPalindrome(string s)
+        {
+            if (s == "")
+                return true;
+            
+            // Remove spaces
+            s.Replace(" ", "");
+
+            // Convert to array in order to reverse
+            char[] forward = s.ToCharArray();
+            char[] backward = forward;
+            
+            Array.Reverse(forward);
+
+            return forward.Equals(backward);
+        }
+    }
+}
