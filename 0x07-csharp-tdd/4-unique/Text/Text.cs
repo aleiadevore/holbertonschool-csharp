8de350a7586f17d351b014718dc7602b20f4048c
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-29 17:56:23 -0500

    Adding messy code, needs comments

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Text
+{
+    public class Str
+    {
+        public static int UniqueChar(string s)
+        {
+            /// use Disctinct to find unique instances
+            List<char> sList = new List<char>();
+            sList.AddRange(s);
+
+            HashSet<char> sHash = new HashSet<char>(sList);
+
+            /// find first instance of new str in old str
+            int i = 0;
+            foreach (char c in s)
+            {
+                if (c == sHash.First())
+                    return i;
+                i++;
+            }
+            return -1;
+        }
+    }
+}
