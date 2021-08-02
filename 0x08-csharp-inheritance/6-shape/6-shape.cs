62d8dcc2724302dd9dcce42a568f5f498e4901dd
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 15:42:45 -0500

    More advanced get and set

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Shape
+{
+    /// Throws and exception
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>Gets and sets the private int width</summary>
+    public int Width
+    {
+        get => width;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+            {
+                width = value;
+            }
+        }
+    }
+
+    /// <summary>Gets and sets the private int height</summary>
+    public int Height
+    {
+        get => height;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+            {
+                height = value;
+            }
+        }
+    }
+}
