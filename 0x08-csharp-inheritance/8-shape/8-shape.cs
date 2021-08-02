db90ed7d327167c35090bc193c4b382310506a92
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 15:59:07 -0500

    References public attribute of parent

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Shape
+{
+    /// Throws and exception
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>Gets and sets the private int width</summary>
+    public int Width
+    {
+        get => width;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+            {
+                width = value;
+            }
+        }
+    }
+
+    /// <summary>Gets and sets the private int height</summary>
+    public int Height
+    {
+        get => height;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    /// <summary>Overrides Area() method in base class to return the area of the rectangle </summary>
+    public new int Area()
+    {
+        return this.width * this.height;
+    }
+
+    /// <summary>Returns string with width and height of rectangle</summary>
+    public override string ToString()
+    {
+        return $"[Rectangle] {this.width} / {this.height}";
+    }
+}
+
+class Square : Rectangle
+{
+    private int size;
+
+    /// <summary>Sets width, height, and size or throws error</summary>
+   public int Size
+   {
+       get => size;
+       set {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+            {
+                Height = value;
+                Width = value;
+                size = value;
+            }
+        }
+   } 
+}
