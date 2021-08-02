927ca6d962d4c96c3307155b312bc65bed121808
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 13:00:40 -0500

    Using GetProperties and GetMethods

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    /// <summary>prints the names of the available properties and methods of an object</summary>
+    public static void Print(object myObj)
+    {
+        var t = myObj.GetType();
+        var i = t.GetTypeInfo();
+        Console.WriteLine("{0} Properties:", i.Name);
+
+        // cycles through properites(in list) and prints each
+       foreach (var property in t.GetProperties())
+            Console.WriteLine(property.Name);
+        
+        Console.WriteLine("{0} Methods:", i.Name);
+
+        // cycles through methods(in list) and prints each
+       foreach (var method in t.GetMethods())
+            Console.WriteLine(method.Name);
+
+    }
+}
