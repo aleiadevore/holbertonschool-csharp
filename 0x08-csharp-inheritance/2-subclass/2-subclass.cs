4bcbb3d8e3d3b637ec3884597dc629cd7177a736
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 12:38:20 -0500

    Determines if type is a subclass of another

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    ///<summary>returns True if the object is an instance of a class that inherits from the specified class, otherwise return False</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
