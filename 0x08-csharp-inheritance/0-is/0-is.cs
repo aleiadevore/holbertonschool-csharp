3999600ab2d8bb02057fdfecfe7f342b6948edc1
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 12:15:25 -0500

    Determines if object is of type int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    /// <summary>Returns True is object is int, otherwise returns False</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType().Equals(typeof(int)))
+            return true;
+        return false;
+    }
+}
