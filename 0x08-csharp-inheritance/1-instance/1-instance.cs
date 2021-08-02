6890fa2e39492de2cfab3350bb8692fd7b70bf99
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 12:31:23 -0500

    Determines if object is instance of array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    ///<summary>Returns true if obj is instance of or inherits from instance of Array</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj))
+            return true;
+        return false;
+    }
+}
