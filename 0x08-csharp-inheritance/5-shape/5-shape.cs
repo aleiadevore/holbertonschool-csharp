19e175fe2efe82d3f4f4cf096bc55a55be9b3fae
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-02 13:06:09 -0500

    Created virtual method

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Shape
+{
+    /// Throws and exception
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
