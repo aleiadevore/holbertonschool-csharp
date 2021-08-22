1c7c86162c38f2605fa16c65028fbe602b6066c0
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-22 14:43:38 -0500

    Adding untested

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MatrixMath
+{
+    ///<summary>Multiplies a matrix by a scalar</summary>///
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int Height = matrix.GetLength(0);
+        int Width = matrix.GetLength(1);
+
+        if (Height < 2 || Height > 3)
+            return new double[,] {{-1}};
+
+        double[,] ans = new double[Height, Width];
+
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+                ans[i, j] = vector[i, j] * scalar;
+        }
+
+        return ans;
+    }
+}
