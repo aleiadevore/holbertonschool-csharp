d01e2da3ff2b342b328cda31b841f0fa647e536b
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-22 12:47:28 -0500

    Program to determine magnitude of vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        double left = 0.00;
+
+        foreach (double item in vector)
+            left += item * item;
+        double a = Math.Sqrt(left);
+        return Math.Round(a, 2);
+    }
+}
