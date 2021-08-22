e12d0eff7d454d9a4fc9a4f8a8367f45cf4d854c
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-22 13:36:09 -0500

    Saving untested

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class VectorMath
+{
+    ///<summary>Multiplies a vector by a scalar</summary>///
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length > 3 || vector.Length < 2)
+            return new double[] {-1};
+
+        double[] ans = new double[vector.Length];
+
+        for (int i = 0; i < vector.Length; i++)
+            ans[i] = vector[i] * scalar;
+
+        return ans;
+    }
+}
