7efa98481c70b75e3708918c19666db411e5cb31
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-22 13:46:42 -0500

    Adding untested

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    ///<summary>Returns dot product of two vectors</summary>///
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length > 3 || vector1.Length < 2)
+            return -1;
+        if (vector1.Length != vector2.Length)
+            return -1;
+
+        double ans = 0;
+
+        for (int i = 0; i < vector1.Length; i++)
+            ans += vector1[i] * vector2[i];
+
+        return ans;
+    }
+}
