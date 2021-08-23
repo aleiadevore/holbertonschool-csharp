2c4f16ede62c6b2b6fcfb5b9867c0046a7199a4a
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-23 14:51:39 -0500

    Calculates inverse of 2d matrix

---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class MatrixMath
+{
+    ///<summary>Calculates inverse of 2D matrix</summary>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
+
+        double denom = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+        if (denom == 0)
+            return new double[,] {{-1}};
+
+        double scalar = 1 / denom;
+
+        double[,] temp = new double[,] { {matrix[1,1], matrix[0, 1] * -1},
+        {matrix[1, 0] * -1, matrix[0, 0]} };
+
+        return MultiplyScalar(temp, scalar);
+    }
+
+    ///<summary>Multiplies a matrix by a scalar</summary>///
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int Height = matrix.GetLength(0);
+        int Width = matrix.GetLength(1);
+
+        if (Height < 2 || Height > 3)
+            return new double[,] {{-1}};
+        if (Width < 2 || Width > 3)
+            return new double[,] {{-1}};
+
+        double[,] ans = new double[Height, Width];
+
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+                ans[i, j] = matrix[i, j] * scalar;
+        }
+
+        return ans;
+    }
+}
