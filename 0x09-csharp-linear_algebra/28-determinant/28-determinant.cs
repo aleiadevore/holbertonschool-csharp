b0bcea8b2bba5b074d2769eee3f69ac1d2018607
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-23 11:46:44 -0500

    Works for 2d only

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        int height = matrix.GetLength(0);
+        int width = matrix.GetLength(1);
+
+        if (height < 2 || height > 3 || width < 0 || width > 3)
+            return -1;
+        
+        if (height != width)
+            return -1;
+
+        if (height == 2)
+            return Det2D(matrix);
+        return Det3D(matrix);
+    }
+
+    ///<summary>Helper function for 2D determinates</summary>
+    public static double Det2D(double[,] m)
+    {
+        return m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0];
+    }
+
+    ///<summary>Helper function for 3D determinates</summary>
+    public static double Det3D(double[,] matrix)
+    {
+        return 0;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/28-determinant/main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[,] m3 = new double[,] { {2, 2}, {-9, 4}};
+        double ans2 = MatrixMath.Determinant(m3);
+        for (int i = 0; i < m3.GetLength(0); i++)
+        {
+            for (int j = 0; j < m3.GetLength(1); j++)
+            {
+                Console.Write(m3[i, j]);
+                if (j != m3.GetLength(1) - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Determinate: {0}", ans2);
+    }
+}
