a06c7e1d6f9eb9f27dda00bf84c47b3329c75a08
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-23 12:48:29 -0500

    Generates cross-product

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MatrixMath
+{
+    ///<summary>Returns cross product of two 3D vectors</summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return new double[] {-1};
+        
+        double x = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+        double y = vector1[0] * vector2[2] - vector1[2] * vector2[0];
+        double z = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+
+        double[] ans = new double[3];
+
+        ans[0] = Math.Round(x, 2);
+        ans[1] = Math.Round(y, 2);
+        ans[2] = Math.Round(z, 2);
+
+        return ans;
+    }
+}
