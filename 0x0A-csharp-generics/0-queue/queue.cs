c659c3800918323247ba4680bb9238b4fa687418
aleiadevore <2496@holbertonschool.com>
2021-10-25 12:08:50 -0500

    Returns type of queue

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Creates generic queue</summary>
+public class Queue<T>
+{
+    ///<summary>Creates method to check type of Queue</summary>
+    public System.Type CheckType()
+    {
+        return typeof(T);
+    }
+}
