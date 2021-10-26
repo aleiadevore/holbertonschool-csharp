ee2e9c9ee3272d64c09c161ce3071caacd943424
aleiadevore <2496@holbertonschool.com>
2021-10-26 10:40:05 -0500

    Created abstract class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>Creates generic abstract class</summary>
+public abstract class Base
+{
+    ///<summary>Name</summary>
+    public string name { get; set; }
+
+    ///<summary>Overrides ToString method</summary>
+    public override string ToString()
+    {
+    
+        return this.name + " is a " + this.GetType();
+    }
+}
