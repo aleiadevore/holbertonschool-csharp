2425ab8f9654d565e07b618896dd4ce2d563f818
aleiadevore <2496@holbertonschool.com>
2021-10-27 10:56:41 -0500

    Creates Player class

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+///<summary>Creates player</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Constructs Player</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    ///<summary>Prints health</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
