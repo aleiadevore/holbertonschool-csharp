3fe4c58b3565eca00cd1b4870b82c2e614a045f5
aleiadevore <2496@holbertonschool.com>
2021-10-27 11:15:14 -0500

    Created delegation

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+///<summary>Creates player</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Constructs Player</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    ///<summary>Delegate for health calculations</summary>
+    public delegate float CalculateHealth(float fl);
+
+    ///<summary>Handles damage</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            damage = 0;
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+        this.hp -= damage;
+    }
+
+    ///<summary>Handles healing</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+            heal = 0;
+        Console.WriteLine($"{this.name} heals {heal} HP");
+        this.hp += heal;
+    }
+
+    ///<summary>Prints health</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
