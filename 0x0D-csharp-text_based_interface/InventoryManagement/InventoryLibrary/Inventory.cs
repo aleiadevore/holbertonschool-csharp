41e8325ba3bcc9e595eaf8fc6a6bb0bdfafb83bd
aleiadevore <2496@holbertonschool.com>
2021-11-02 12:07:04 -0500

    Added JSON

--- 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/Inventory.cs
+++ 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/Inventory.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 namespace InventoryLibrary
 {
-    public class Inventory
+    public class Inventory : BaseClass
     {
-        public Inventory()
+        public string user_id;
+        public string item_id;
+        public int quantity;
+
+        public Inventory(string user_id, string item_id, int quantity=1)
         {
+            this.user_id = user_id;
+            this.item_id = item_id;
+            if (quantity < 0)
+                this.quantity = 1;
+            else
+                this.quantity = quantity;
         }
     }
 }
---
+++ 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/JSONStorage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+namespace InventoryLibrary
+{
+    public class JSONStorage
+    {
+        /// <summary>
+        /// Dictionary of all BaseClass objects
+        /// Key is ClassName.id
+        /// </summary>
+        public Dictionary<string, BaseClass> objects;
+
+        public JSONStorage(Dictionary<string, BaseClass> objects)
+        {
+            this.objects = objects;
+        }
+
+        /// <summary>
+        /// Return objects dictionary
+        /// </summary>
+        public Dictionary<string, BaseClass> All()
+        {
+            return objects;
+        }
+
+        /// <summary>
+        /// Add a new key-value pair to objects
+        /// </summary>
+        /// <param name="obj">Object to add to objects dictionary</param>
+        public void New(BaseClass obj)
+        {
+            Type ClassName = obj.GetType();
+            string id = obj.id;
+
+            string key = $"{ClassName}.{id}";
+
+            objects.Add(key, obj);
+        }
+
+        /// <summary>
+        /// Serializes objects to JSON and saves to JSON file
+        /// </summary>
+        public void Save()
+        {
+            string json = JsonSerializer.Serialize(objects);
+            File.WriteAllText("storage/inventory_manager.json", json);
+        }
+
+        /// <summary>
+        /// Deserializes JSON file to objects dictionary
+        /// </summary>
+        public void Load()
+        {
+            string fileName = "storage/inventory_manager.json";
+            string jsonString = File.ReadAllText(fileName);
+            objects = JsonSerializer.Deserialize<Dictionary<string, BaseClass>>(jsonString);
+        }
+    }
+}
